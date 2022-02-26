223760e97b7828478a734cc6501a96e1c9a5038f
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-26 16:44:36 +0700

    Сделал усложненное задание 21 урока

--- src/modules/valid.js
+++ src/modules/valid.js
@@ -16,39 +16,87 @@ const valid = () => {
     const calcCount = document.querySelector('.calc-count')
     const calcDay = document.querySelector('.calc-day')
 
+    const blurInput = (ex) => {
+        ex.target.value = ex.target.value.replace(/[\s]{2,}/, " ")
+        ex.target.value = ex.target.value.replace(/[\-]{2,}/, "-")
+        ex.target.value = ex.target.value.replace(/^\-/, "")
+        ex.target.value = ex.target.value.replace(/\-$/, "")
+    }
+    
     form1Name.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "")
+        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/g, "")
     })
+    form1Name.addEventListener('blur', (e) => {
+        blurInput(e)
+        e.target.value = e.target.value.toString().substring('0', '1').toUpperCase() + e.target.value.toString().substring('1').toLowerCase()
+    })
+
     form1Email.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/, "")
+        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/g, "")
     })
+    form1Email.addEventListener('blur', (e) => {
+        blurInput(e)
+    })
+
     form1Phone.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/, "")
+        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/g, "")
+    })
+    form1Phone.addEventListener('blur', (e) => {
+        blurInput(e)
     })
 
 
     form2Name.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "")
+        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/g, "")
     })
+    form2Name.addEventListener('blur', (e) => {
+        blurInput(e)
+        e.target.value = e.target.value.toString().substring('0', '1').toUpperCase() + e.target.value.toString().substring('1').toLowerCase()
+    })
+
     form2Message.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "")
+        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/g, "")
     })
+    form2Message.addEventListener('blur', (e) => {
+        blurInput(e)
+        e.target.value = e.target.value.toString().substring('0', '1').toUpperCase() + e.target.value.toString().substring('1').toLowerCase()
+    })
+
     form2Email.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/, "")
+        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/g, "")
     })
+    form2Email.addEventListener('blur', (e) => {
+        blurInput(e)
+    })
+
     form2Phone.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/, "")
+        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/g, "")
+    })
+    form2Phone.addEventListener('blur', (e) => {
+        blurInput(e)
     })
 
 
     form3Name.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/, "")
+        e.target.value = e.target.value.replace(/[^А-Яа-я\s\-]+/g, "")
     })
+    form3Name.addEventListener('blur', (e) => {
+        blurInput(e)
+        e.target.value = e.target.value.toString().substring('0', '1').toUpperCase() + e.target.value.toString().substring('1').toLowerCase()
+    })
+
     form3Email.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/, "")
+        e.target.value = e.target.value.replace(/[^A-Za-z0-9\@\-\_\.\!\`\*\']+/g, "")
     })
+    form3Email.addEventListener('blur', (e) => {
+        blurInput(e)
+    })
+
     form3Phone.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/, "")
+        e.target.value = e.target.value.replace(/[^0-9\(\)\-]+/g, "")
+    })
+    form3Phone.addEventListener('blur', (e) => {
+        blurInput(e)
     })
 
 
