191900af2dcdf3c4479af2bae39079e887359f6c
Vlad Vladimirov <blad20002000@mail.ru>
2022-03-01 16:46:46 +0700

    Сделал усложненное задание 23 урока

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,17 +1,22 @@
-const slider = () => {
-    const sliderBlock = document.querySelector('.portfolio-content')
-    const slides = document.querySelectorAll('.portfolio-item')
-    let dots = document.querySelectorAll('.dot')
+const slider = (classBlock, classSlides, classActiveSlide = 'portfolio-item-active', classActiveDot = 'dot-active') => {
+    const sliderBlock = document.querySelector(classBlock)
+    const slides = document.querySelectorAll(classSlides)
     const portfolioDots = document.querySelector('.portfolio-dots')
-
+    let dots
+    
     document.addEventListener("DOMContentLoaded", () => {
-        for (let i = 0; i < slides.length; i++) {
-            let li = document.createElement('li')
-            li.classList.add('dot')
-            if(i == 0) {li.classList.add('dot-active')}
-            portfolioDots.appendChild(li)
+        if (sliderBlock == null || slides == null) {
+            console.log('error');
+        } else {
+            for (let i = 0; i < slides.length; i++) {
+                let li = document.createElement('li')
+                li.classList.add('dot')
+                if(i == 0) {li.classList.add(classActiveDot)}
+                portfolioDots.appendChild(li)
+            }
+            dots = document.querySelectorAll('.dot')
+            startSlide(timeInterval)
         }
-        dots = document.querySelectorAll('.dot')
     });
 
     const timeInterval = 2000
@@ -27,16 +32,16 @@ const slider = () => {
     }
 
     const autoSlide = () => {
-        prevSlide(slides, currentSlide, 'portfolio-item-active')
-        prevSlide(dots, currentSlide, 'dot-active')
+        prevSlide(slides, currentSlide, classActiveSlide)
+        prevSlide(dots, currentSlide, classActiveDot)
 
         currentSlide++
         if (currentSlide >= slides.length) {
             currentSlide = 0
         }
 
-        nextSlide(slides, currentSlide, 'portfolio-item-active')
-        nextSlide(dots, currentSlide, 'dot-active')
+        nextSlide(slides, currentSlide, classActiveSlide)
+        nextSlide(dots, currentSlide, classActiveDot)
     }
     const startSlide = (timer = 1500) => {
         interval = setInterval(autoSlide, 2000)
@@ -52,8 +57,8 @@ const slider = () => {
             return
         }
 
-        prevSlide(slides, currentSlide, 'portfolio-item-active')
-        prevSlide(dots, currentSlide, 'dot-active')
+        prevSlide(slides, currentSlide, classActiveSlide)
+        prevSlide(dots, currentSlide, classActiveDot)
 
         if(e.target.matches('#arrow-right')) {
             currentSlide++
@@ -74,8 +79,8 @@ const slider = () => {
             currentSlide = slides.length - 1
         }
 
-        nextSlide(slides, currentSlide, 'portfolio-item-active')
-        nextSlide(dots, currentSlide, 'dot-active')
+        nextSlide(slides, currentSlide, classActiveSlide)
+        nextSlide(dots, currentSlide, classActiveDot)
     })
 
     sliderBlock.addEventListener('mouseenter', (e) => {
@@ -88,8 +93,6 @@ const slider = () => {
             startSlide(timeInterval)
         }
     }, true)
-
-    startSlide(timeInterval)
 }
 
 export default slider
